55ed764b039a3717189aad93568675358bc50ec2
mirandaunwin <miranda.unwin@gmail.com>
2018-11-15 13:16:27 +0100

    heroku still throwing errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  
+
 
   def index
     @articles = Article.all
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def create
